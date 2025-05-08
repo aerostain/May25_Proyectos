ad76238ebf9e99a56cfe7ff486f6952c2af93918
r1ck7 <richims026@gmail.com>
2025-05-08 13:19:35 -0500

    Upd_1

--- Productividad_Prosegur/Storage/main.sql
+++ Productividad_Prosegur/Storage/main.sql
@@ -4,7 +4,44 @@ create database st_prosegur_080525;
 use st_prosegur_080525;
 
 # Crear Tablas
-
+-- 
+create table permisos(
+id int primary key auto_increment,
+permisos int not null,
+descripcion varchar(250) not null,
+fec_actu timestamp,
+fec_crea timestamp default current_timestamp()
+);
+-- 
+create table pass(
+id int primary key auto_increment,
+id_personal int not null,
+pass varchar(250) not null,
+fec_actu timestamp,
+fec_crea timestamp default current_timestamp()
+);
+-- 
+create table personal_rrhh(
+id int primary key auto_increment,
+nombre  varchar(250) not null,
+apellido_p  varchar(250) not null,
+apellido_m  varchar(250) not null,
+puesto  varchar(250) not null,
+cargo  varchar(250) not null,
+negocio_ensucontrato int not null,
+negocio_entornolaboral int not null,
+permisos int not null,
+fec_actu timestamp,
+fec_crea timestamp default current_timestamp()
+);
+-- 
+create table negocios(
+id int primary key auto_increment,
+n_negocio varchar(250),
+fec_actu timestamp,
+fec_crea timestamp default current_timestamp()
+);
+-- 
 
 
 show tables;
---
+++ Productividad_Prosegur/readme.md
@@ -0,0 +1,25 @@
+#
+
+###
+
+### Mysql
+* Para actualizar un registro luego de hacer un `insert`, por ejemplo, usar trigger, pero lo mejor es hacerlo desde la aplicación:
+```sql
+-- 1. Agregar la columna 'codigo' a la tabla 'negocios'
+ALTER TABLE negocios
+ADD COLUMN codigo VARCHAR(255);
+
+-- 2. Crear el trigger que se ejecuta DESPUÉS de cada inserción
+DELIMITER //
+CREATE TRIGGER generar_codigo_negocio
+AFTER INSERT ON negocios
+FOR EACH ROW
+BEGIN
+    -- Actualizar la columna 'codigo' de la nueva fila insertada
+    UPDATE negocios
+    SET codigo = CONCAT('NEG', NEW.id, DATE_FORMAT(NEW.fec_crea, '%Y%m%d%H%i%s'))
+    WHERE id = NEW.id;
+END;
+//
+DELIMITER ;
+```
